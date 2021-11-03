a86916e2d8cec10e295accfe76cb51c0d0114683
yossy-infratop <yoshikawa.akito.infratop@gmail.com>
2021-11-03 07:45:36 +0000

    [finish]chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '09970a6e377e012ce7ec0e88610512bfcc87835d23d17e38d27df7921abf11e124da36cffdb84fca02a209a7e94249ca3981d9a02a4de7707cb1c33a66acdc04'
